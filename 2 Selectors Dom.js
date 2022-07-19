7585fca0f0637e22b5ae0fff72f5ee0583074a2c
kamigella <kamillamagella@gmail.com>
2022-07-19 14:57:34 +0200

    selectors

---
+++ 2 Selectors Dom.js
@@ -0,0 +1,16 @@
+
+//Slectionner Class
+let i = document.getElementsByClassName("important");
+console.log(i);
+
+/*
+for (i=0; i<notesNum; i++){
+    notes[i].setatribute('title')
+}
+
+let x = querySelectorAll("important");
+console.log(x);
+
+Selectionner Tag 
+let p = document.getElementsByTagName("p");
+console.log(p); */
